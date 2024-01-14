7eea1e71eaf559dfe1b9375351298fb609b3c3dc
hassan_adnan <adnanhassanda@gmail.com>
2024-01-15 02:19:49 +0800

    modify conn

--- App/pages/payment_selection.php
+++ App/pages/payment_selection.php
@@ -8,8 +8,8 @@ if (!isset($_SESSION['user_id'])) {
     header("Location: login.php");
     exit;
 }
-
-$conn = mysqli_connect('localhost', 'root', '', 'u733671518_project');
+$conn = mysqli_connect('127.0.0.1:3306','u733671518_wibs','|4Kh/3XYD','u733671518_project');
+#$conn = mysqli_connect('localhost', 'root', '', 'u733671518_project');
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
