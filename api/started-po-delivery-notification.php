7928c89503dbc02cc160ef0dc489b5b577c34dc3
hassan_adnan <adnanhassanda@gmail.com>
2024-01-17 00:33:05 +0800

    update started po delivery notif

--- api/started-po-delivery-notification.php
+++ api/started-po-delivery-notification.php
@@ -3,7 +3,7 @@ header("Access-Control-Allow-Origin:*");
 header("Access-Control-Allow-Methods:*");
 header("Access-Control-Allow-Headers:*");
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+/* if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Retrieve POST parameters
     $po_id = isset($_POST['po_id']) ? $_POST['po_id'] : null;
     $delivery_reference_number = isset($_POST['delivery_reference_number']) ? $_POST['delivery_reference_number'] : null;
@@ -69,4 +69,43 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     http_response_code(405);
     echo json_encode(['error' => 'Method Not Allowed.']);
 }
-?>
+?>*/
+
+$conn = mysqli_connect('127.0.0.1:3306','u733671518_wibs','|4Kh/3XYD','u733671518_project');
+#$conn = mysqli_connect('localhost','root','','u733671518_project');
+
+if (!$conn) {
+    die("Connection Failed: " . mysqli_connect_error());
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['po_id'], $_POST['delivery_reference_number'])) {
+
+        $poId = $_POST['po_id'] ?? "";
+        $deliveryReferenceNumber = $_POST['delivery_reference_number'] ?? "";
+
+        // Begin transaction
+        mysqli_begin_transaction($conn);
+
+        // Insertion query
+        $insertSql = "INSERT INTO order_delivery_info (po_id, delivery_reference_number) VALUES ('$poId', '$deliveryReferenceNumber')";
+        if (!mysqli_query($conn, $insertSql)) {
+            echo "Error: " . $insertSql . "<br>" . mysqli_error($conn);
+            mysqli_rollback($conn);
+        } else {
+            // Update query
+            $updateSql = "UPDATE purchase_order SET status = 3 WHERE id = '$poId'";
+            if (!mysqli_query($conn, $updateSql)) {
+                echo "Error: " . $updateSql . "<br>" . mysqli_error($conn);
+                mysqli_rollback($conn);
+            } else {
+                // Commit transaction
+                mysqli_commit($conn);
+                echo "New record created successfully and po_id status updated";
+            }
+        }
+    }
+    mysqli_close($conn);
+}
+
+
