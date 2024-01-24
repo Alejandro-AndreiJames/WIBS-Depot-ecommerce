1a18505ed885d1e1390e0848e695024581bf7101
Andrei James Alejandro <squishycaptain09@gmail.com>
2024-01-25 02:52:22 +0800

    add view profile and logout functionality for homepage

--- App/js/homepage.js
+++ App/js/homepage.js
@@ -189,3 +189,22 @@ document.addEventListener('DOMContentLoaded', function() {
     displayInitialItems();
     fetchItems(5, itemsPerPage).then(displayAllDeals).catch(error => console.error('Error:', error));
 });
+
+
+// Function to toggle the dropdown
+function toggleDropdown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    for (var i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
